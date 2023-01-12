a596a7a50db5395c121d6060e5c8bb94379d33da
hsyen23 <94521561+hsyen23@users.noreply.github.com>
2023-01-11 23:35:10 -0800

    Delete find_second_large.m

--- ECE271A_Statistical Learning I/PR1/find_second_large.m
+++
@@ -1,7 +0,0 @@
-function x_index = find_second_large(data)
-    for index = 1 : size(data)
-        arr = abs(data);
-        x_value = max(arr(arr<max(arr)));
-        x_index = find(arr == x_value);
-    end
-end
